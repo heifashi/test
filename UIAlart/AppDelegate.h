3f83aa00be16871fc76b7bf85bc949840d7b5b4c
黑法师 <heifashi@dianjia.io>
2016-07-18 09:56:41 +0800

    Initial Commit

---
+++ UIAlart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIAlart
+//
+//  Created by 黑法师 on 16/7/18.
+//  Copyright © 2016年 黑法师. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIAlart/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIAlart
+//
+//  Created by 黑法师 on 16/7/18.
+//  Copyright © 2016年 黑法师. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
